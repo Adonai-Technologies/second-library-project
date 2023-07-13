1a3fcd196faedb763a39a262302f2e066b14a004
Heavenba7 <akuakkonadu8@gmail.com>
2023-07-13 15:27:38 +0100

    started

---
+++ script.js
@@ -0,0 +1,18 @@
+let pEl= document.getElementById("p-el")
+
+let myLibrary = [];
+
+function Book(title,author,page){
+    this.title = title;
+    this.author = author;
+    this.page = page
+    this.toString = toString
+}
+
+
+
+function addBookToLibrary() {
+ 
+
+
+}
