0fba42d739cc6f7b00cf853bb4ffb3a3d24c1767
Heavenba7 <akuakkonadu8@gmail.com>
2023-07-18 20:18:53 +0100

    new updates

---
+++ myscript.js
@@ -0,0 +1,20 @@
+
+let myLibrary = [];
+
+
+
+function Book(){
+    this.title= "BREAK",
+    this.author = "AKUA",
+    this.page = 365
+}
+
+let myNewBook = new Book()
+  
+
+
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
