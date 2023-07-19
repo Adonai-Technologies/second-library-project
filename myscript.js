265303fca7ee9a1304f2a44870cd45985648bd41
Heavenba7 <akuakkonadu8@gmail.com>
2023-07-19 16:46:39 +0100

    starting my library with the boilerplates

--- myscript.js
+++ myscript.js
@@ -1,8 +1,1 @@
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
