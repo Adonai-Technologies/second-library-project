ec69489c459f331ebed4446f7d75c6480954a953
Heavenba7 <akuakkonadu8@gmail.com>
2023-07-19 17:38:21 +0100

    ADADD

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,20 @@
 
-let myLibraryList = [];
+const title = document.querySelector('#title').value;
+const author = document.querySelector('#author').value;
+const pages = document.querySelector('#pages').value;
+const isRead = document.querySelector('#isRead').value;
+const form = document.querySelector('#form');
+form.addEventListener('submit', addBookToLibrary);
 
+
+const addBookBtn = document.querySelector('.add-book-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+addBookBtn.addEventListener('click', function() {
+  modalOverlay.classList.add('overlay-active');
+});
+
+
+let myLibrary = [];
 
 function Book(title,author,pages){
 this.title = title;
@@ -9,9 +23,9 @@ this.pages = pages;
 
 }
 
-function addBook(){
-    
+function addBookToLibrary() {
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
 }
-
 
 
