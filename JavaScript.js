f22e9050e32b2b931ec76fec34967df92f01dc94
Heavenba7 <akuakkonadu8@gmail.com>
2023-07-31 17:54:24 +0100

    ADDED SOME SCRIT TO ADD NEW BOOKS TO THE LIBRARY

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,31 @@
+let myLibrary = [];
+
+let tdEl1 = document.getElementById("td-el1")
+let addBookBtn = document.querySelector("#add-book-btn")
+
+
+
+function Book(title, author, pages) {
+    this.title = title,
+        this.author = author,
+        this.pages = pages
+    this.info = function () {
+        this.title + "by" + " " + this.autor + "with" + " " + this.pages + " " + " not read yet"
+    }
+}
+let myNewBook = new Book("The run away Bride", "Jane Williams", "345")
+
+
+
+
+function addBookToLibrary() {
+    let bookTitleInput = document.getElementById("myBookTitle").value;
+    tdEl1.innerHTML = bookTitleInput
+
+}
+
+
+
 
 
 
